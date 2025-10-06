55c9b99197117851ce3519dc645ab3a066774e28
GiftedR <kidsgamers@hotmail.com>
2025-10-05 21:38:08 -0700

    feat: Added ArrayStringListHelpers Class

---
+++ DataStructuresToolkit/ArrayStringListHelpers.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace DataStructuresToolkit;
+
+public class ArrayStringListHelpers
+{
+	public static int[] InsertIntoArray(int[] arr, int index, int value)
+	{
+		return arr;
+	}
+	
+	public static int[] DeleteFromArray(int[] arr, int index, int value)
+	{
+		return arr;
+	}
+	
+	public static string ConcatenateNamesNaive(string[] names)
+	{
+		return "";
+	}
+	
+	public static string ConcatenateNamesBuilder(string[] names)
+	{
+		return "";
+	}
+	
+	public static List<int> InsertIntoList(List<int> list, int index, int value)
+	{
+		return list;
+	}
+}
