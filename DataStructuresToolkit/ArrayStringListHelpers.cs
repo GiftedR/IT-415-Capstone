61e0522092ef6ba2e5eb9e560bbf06d4e4e14892
GiftedR <kidsgamers@hotmail.com>
2025-10-05 21:44:32 -0700

    feat: Implemented required methods

--- DataStructuresToolkit/ArrayStringListHelpers.cs
+++ DataStructuresToolkit/ArrayStringListHelpers.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace DataStructuresToolkit;
 
@@ -6,26 +7,44 @@ public class ArrayStringListHelpers
 {
 	public static int[] InsertIntoArray(int[] arr, int index, int value)
 	{
+		for (int idx = arr.Length - 1; idx > index; idx--)
+		{
+			arr[idx] = arr[idx - 1];
+		}
+		arr[index] = value;
 		return arr;
 	}
-	
+
 	public static int[] DeleteFromArray(int[] arr, int index, int value)
 	{
+		for (int idx = index; idx < arr.Length; idx++)
+		{
+			arr[idx] = arr[idx + 1];
+		}
 		return arr;
 	}
-	
+
 	public static string ConcatenateNamesNaive(string[] names)
 	{
-		return "";
+		string outputstr = "";
+
+		foreach (string name in names)
+			outputstr += name + ", ";
+
+		return outputstr;
 	}
-	
+
 	public static string ConcatenateNamesBuilder(string[] names)
 	{
-		return "";
+		StringBuilder sb = new();
+		foreach (string name in names)
+			sb.Append(name + ", ");
+		return sb.ToString();
 	}
-	
+
 	public static List<int> InsertIntoList(List<int> list, int index, int value)
 	{
+		list.Insert(index, value);
 		return list;
 	}
 }
