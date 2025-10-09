063dec64129f3960e1037320b2c93ff8008624ce
GiftedR <kidsgamers@hotmail.com>
2025-10-09 09:39:35 -0700

    docs: Added documentation comments to custom stack and queue implementations.

--- DataStructuresToolkit/StackQueues/CustomQueue.cs
+++ DataStructuresToolkit/StackQueues/CustomQueue.cs
@@ -1,5 +1,9 @@
 namespace DataStructuresToolkit.StackQueues;
 
+/// <summary>
+/// Custom implementation of the built in queue.
+/// </summary>
+/// <typeparam name="T">The type of item the queue can hold.</typeparam>
 public class CustomQueue<T>
 {
 	private T[] _items;
@@ -7,13 +11,36 @@ public class CustomQueue<T>
 	private int _head;
 	private int _tail;
 
+	/// <summary>
+	/// Shows the number of items the queue can hold.
+	/// </summary>
 	public int Capacity { get => _items.Length; }
+	/// <summary>
+	/// The amount of items currently in the queue.
+	/// </summary>
 	public int Count { get => _tail - _head; }
 
+	/// <summary>
+	/// Creates a queue with the default capacity.
+	/// </summary>
 	public CustomQueue() : this(25) { }
+
+	/// <summary>
+	/// Creates a queue with the specified capacity.
+	/// </summary>
+	/// <param name="capacity">The starting capactiy of the queue</param>
 	public CustomQueue(uint capacity = minimumItems) { _items = new T[capacity]; }
+
+	/// <summary>
+	/// Creates a queue with a starting set of items.
+	/// </summary>
+	/// <param name="items">The items that start within the queue.</param>
 	public CustomQueue(T[] items) { _items = items; }
 
+	/// <summary>
+	/// Adds a new item to the queue.
+	/// </summary>
+	/// <param name="newitem">The item that gets put at the back of the queue.</param>
 	public void Enqueue(T newitem)
 	{
 		if (_tail == Capacity)
@@ -25,6 +52,11 @@ public class CustomQueue<T>
 		_tail++;
 	}
 
+	/// <summary>
+	/// Removes an item from the queue.
+	/// </summary>
+	/// <returns>The item at the from of the queue.</returns>
+	/// <exception cref="InvalidOperationException">Thrown when the queue is empty.</exception>
 	public T Dequeue()
 	{
 		if (Count == 0)
@@ -35,6 +67,11 @@ public class CustomQueue<T>
 		return dqitem;
 	}
 
+	/// <summary>
+	/// Views the next item in the queue.
+	/// </summary>
+	/// <returns>The next item in the queue.</returns>
+	/// <exception cref="InvalidOperationException">Thrown when the queue is empty.</exception>
 	public T Peek() => Count > 0 ? _items[_head] : throw new InvalidOperationException("Unable to peek an empty queue.");
 
 	private void _ShiftItems()
--- DataStructuresToolkit/StackQueues/CustomStack.cs
+++ DataStructuresToolkit/StackQueues/CustomStack.cs
@@ -1,18 +1,45 @@
-namespace DataStructuresToolkit.StackQueues;
+namespace DataStructuresToolkit.Stackstacks;
 
+/// <summary>
+/// Custom implementation of the built in stack.
+/// </summary>
+/// <typeparam name="T">The type of item the stack can hold.</typeparam>
 public class CustomStack<T>
 {
 	private T[] _items;
 	private const int minimumItems = 25;
 	private int _count = 0;
 
+	/// <summary>
+	/// Shows the number of items the stack can hold.
+	/// </summary>
 	public int Capacity { get => _items.Length; }
+	/// <summary>
+	/// The amount of items currently in the stack.
+	/// </summary>
 	public int Count { get => _count; }
 
+	/// <summary>
+	/// Creates a stack with the default capacity.
+	/// </summary>
 	public CustomStack() : this(25) { }
+
+	/// <summary>
+	/// Creates a stack with the specified capacity.
+	/// </summary>
+	/// <param name="capacity">The starting capactiy of the stack</param>
 	public CustomStack(uint capacity = minimumItems) { _items = new T[capacity]; }
+
+	/// <summary>
+	/// Creates a stack with a starting set of items.
+	/// </summary>
+	/// <param name="items">The items that start within the stack.</param>
 	public CustomStack(T[] items) { _items = items; }
 
+	/// <summary>
+	/// Adds a new item to the top of the stack.
+	/// </summary>
+	/// <param name="newItem">The item to be put on the stack.</param>
 	public void Push(T newItem)
 	{
 		if (_count == _items.Length)
@@ -21,6 +48,11 @@ public class CustomStack<T>
 		_count++;
 	}
 
+	/// <summary>
+	/// Removes an item from the stack.
+	/// </summary>
+	/// <returns>The item on the top of the stack.</returns>
+	/// <exception cref="InvalidOperationException">Thrown when the stack is empty.</exception>
 	public T Pop()
 	{
 		if (_count == 0)
@@ -31,5 +63,10 @@ public class CustomStack<T>
 		return popitem;
 	}
 
+	/// <summary>
+	/// Views the next item on the stack.
+	/// </summary>
+	/// <returns>The next item on the stack.</returns>
+	/// <exception cref="InvalidOperationException">Thrown when the stack is empty.</exception>
 	public T Peek() => _count > 0 ? _items[_count - 1] : throw new InvalidOperationException("Unable to peek an empty stack.");
 }
