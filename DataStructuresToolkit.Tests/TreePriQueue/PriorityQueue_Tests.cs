54986f569ff9fa03efda8fb0c537c96419025c94
GiftedR <kidsgamers@hotmail.com>
2025-12-06 00:05:35 -0800

    feat: Added int priority queue test: Added IPQ tests docs: Added documentation comments with complexity.

--- DataStructuresToolkit.Tests/TreePriQueue/PriorityQueue_Tests.cs
+++ DataStructuresToolkit.Tests/TreePriQueue/PriorityQueue_Tests.cs
@@ -1,6 +1,34 @@
+using DataStructuresToolkit.TreePriQUeue;
+
 namespace DataStructuresToolkit.Tests.TreePriQueue;
 
+[TestFixture]
 public class PriorityQueue_Tests
 {
+	[Test]
+	public void EnqueueShouldAddValue()
+	{
+		IntPriorityQueue ipq = new();
+
+		ipq.Enqueue(3);
+
+		Assert.That(ipq.Dequeue(), Is.EqualTo(3));
+	}
 	
+	private static readonly object[] _sourceLists =
+	{
+		new object[] { new List<int>{5, 2, 8}, 2 },
+		new object[] { new List<int>{6372, 98, 127389}, 98 }
+	};
+	
+	[TestCaseSource(nameof(_sourceLists))]
+	public void EnqueueingAscendingValuesShouldSwapOrder(List<int> queueValues, int minimumValue)
+	{
+		IntPriorityQueue ipq = new();
+
+		foreach (int item in queueValues)
+			ipq.Enqueue(item);
+
+		Assert.That(ipq.Dequeue(), Is.EqualTo(minimumValue));
+	}
 }
--- DataStructuresToolkit/TreePriQueue/PriorityQueue.cs
+++ DataStructuresToolkit/TreePriQueue/PriorityQueue.cs
@@ -1,14 +1,82 @@
 namespace DataStructuresToolkit.TreePriQUeue;
 
-public class PriorityQueue<T>
+/// <summary>
+/// Custom priority queue with ints. Had to be renamed to not conflict with native type.
+/// </summary>
+public class IntPriorityQueue
 {
-	private List<PQItem<T>> _pqItems = new();
-}
+	/// <summary>
+	/// Internal heap storage.
+	/// </summary>
+	private List<int> pq_Heap = new();
 
-internal struct PQItem<T>
-{
-	#pragma warning disable CS0649
-	public int priority;
-	public T item;
-	#pragma warning restore CS0649
+	/// <summary>
+	/// Adds an item to the queue.
+	/// </summary>
+	/// <param name="newValue">The item to add, also acts as a priority.</param>
+	/// <remarks>
+	/// 	<complexity>
+	/// 		Time: O(log n) - Splits heap traversals for optimization.
+	/// 		Space: O(1) - Only creates an index regardless of heap size.
+	/// 	</complexity>
+	/// </remarks>
+	public void Enqueue(int newValue)
+	{
+		pq_Heap.Add(newValue);
+		int idx = pq_Heap.Count - 1;
+		while (idx > 0 && pq_Heap[(idx-1)/2] > pq_Heap[idx])
+		{
+			(pq_Heap[idx], pq_Heap[(idx-1)/2]) = (pq_Heap[(idx-1)/2], pq_Heap[idx]); // Index Swapping
+			idx = (idx-1)/2;
+		}
+	}
+	
+	/// <summary>
+	/// Removes the item from the top of the queue.
+	/// </summary>
+	/// <returns>The highest priority item.</returns>
+	/// <exception cref="InvalidOperationException">Thrown if the heap is empty when attempting to dequeue.</exception>
+	/// <remarks>
+	/// 	<complexity>
+	/// 		Time: O(1) - Grabs the first item directly.
+	/// 		Space: O(1) - Only holds reference to the front of the queue.
+	/// 	</complexity>
+	/// </remarks>
+	public int Dequeue()
+	{
+		if (pq_Heap.Count == 0)
+			throw new InvalidOperationException("Heap cannot be empty.");
+		int root = pq_Heap[0];
+		pq_Heap[0] = pq_Heap[^1];
+		pq_Heap.RemoveAt(pq_Heap.Count - 1);
+		Heapify(0);
+		return root;
+	}
+
+	/// <summary>
+	/// Sorts the heap to fit the expected format.
+	/// </summary>
+	/// <param name="index">The lower bounds to index from/</param>
+	/// <remarks>
+	/// 	<complexity>
+	/// 		Time: O(log n) - Performs a type of sorting.
+	/// 		Space: O(1) - Only holds 3 indexes regardless of size.
+	/// 	</complexity>
+	/// </remarks>
+	private void Heapify(int index)
+	{
+		int smallest = index;
+		int left_index = 2 * index + 1;
+		int right_index = 2 * index + 2;
+
+		if (left_index < pq_Heap.Count && pq_Heap[left_index] < pq_Heap[smallest])
+			smallest = left_index;
+		if (right_index < pq_Heap.Count && pq_Heap[right_index] < pq_Heap[smallest])
+			smallest = right_index;
+
+		if (smallest != index) {
+			(pq_Heap[index], pq_Heap[smallest]) = (pq_Heap[smallest], pq_Heap[index]);
+			Heapify(smallest);
+		}
+	}
 }
